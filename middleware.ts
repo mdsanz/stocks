8625441424d3d56b7507a84fb450742d825c4e48
mdsanz <marcos.sanzlara@gmail.com>
2026-03-30 21:09:20 -0600

    fix auth logic

--- middleware.ts
+++ middleware.ts
@@ -2,9 +2,9 @@ import { NextRequest, NextResponse } from 'next/server';
 import { getSessionCookie } from "better-auth/cookies";
 
 export async function middleware(request: NextRequest) {
-const sessionCookie = getSessionCookie(request);
+  const sessionCookie = getSessionCookie(request);
 
- // Check cookie presence - prevents obviously unauthorized users
+  // Check cookie presence - prevents obviously unauthorized users
   if (!sessionCookie) {
     return NextResponse.redirect(new URL('/sign-in', request.url));
   }
