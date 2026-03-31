d2382efa67813e1464600cd42c748847b833ceff
mdsanz <marcos.sanzlara@gmail.com>
2026-03-30 19:22:29 -0600

    implement middleware

--- lib/inngest/functions.ts
+++ lib/inngest/functions.ts
@@ -16,7 +16,7 @@ export const sendSignUpEmail = inngest.createFunction(
         const prompt = PERSONALIZED_WELCOME_EMAIL_PROMPT.replace('{{userProfile}}', userProfile)
 
         const response = isAIEnabled ? await step.ai.infer("generate-welcome-intro", {
-            model: step.ai.models.gemini({ model: 'gemini-1.5-flash' }),
+            model: step.ai.models.gemini({ model: 'gemini-2.5-flash' }),
             body: {
                 contents: [
                     {
